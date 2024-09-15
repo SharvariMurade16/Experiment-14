2efd6ac5398287508cd7750eb3f1a83f216bf3ef
SharvariMurade16 <sharvari.murade.btech2023@sitpune.edu.in>
2024-09-15 23:02:59 +0530

    Create factorial.cpp

---
+++ factorial.cpp
@@ -0,0 +1,21 @@
+//sharvari murade
+//23070123088
+#include<iostream>
+using namespace std;
+
+int factorial(int n);
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    cout << "Factorial of " << n << " = " << factorial(n);
+    return 0;
+}
+
+int factorial(int n) {
+    if(n==0){
+        return 1;
+    } else{
+        return n*factorial(n - 1);
+    }
+}
