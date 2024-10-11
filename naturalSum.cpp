c23ce91578a7575ee06d819db886a28a8f15ef03
SharvariMurade16 <sharvari.murade.btech2023@sitpune.edu.in>
2024-10-11 08:56:28 +0530

    Create naturalSum.cpp

---
+++ naturalSum.cpp
@@ -0,0 +1,29 @@
+//sharvari murade
+//23070123088
+# include<iostream>
+using namespace std;
+int sum(int n)
+{
+    if(n==0)
+    {
+        cout<< " Number should be greater than 1"<<endl;
+    }
+    if(n==1)
+    {
+        return 1;
+    }
+    else
+    {
+     return (n+sum(n-1));
+
+    }
+}
+    int main()
+{
+    int f,n;
+    cout << "Enter a number : "<<endl;
+    cin >> n ;
+
+    cout <<"Sum of numbers from 1 to "<<n<<" is: "<<" : "<<sum(n) ;
+ 
+}
